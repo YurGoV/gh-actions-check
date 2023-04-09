e9e28d512f1a83ee7dbaf803d3d46e8be4308d3d
Yuriy Gomenyuk <yourgo@gmail.com>
2023-04-09 20:53:25 +0300

    deploy-draft-setup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from 'redux-persist/integration/react';
 import {store, persistor} from "./redux/store";
 
-
 import {App} from 'components/App';
 import {Provider} from "react-redux";
 import './index.css';

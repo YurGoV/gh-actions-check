d0a818c07437ae1a1abaa36494a9973c8d4e286b
Yuriy Gomenyuk <yourgo@gmail.com>
2023-04-09 23:41:24 +0300

    m2

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='goit-react-hw-08-phonebook'>
